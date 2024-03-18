4c72c36d8d2f1eb48f1e43bed077ed6f8e01a18e
jujube6363 <113395821+jujube6363@users.noreply.github.com>
2024-03-18 17:49:10 -0500

    Update StringSubsequence.c

--- StringSubsequence.c
+++ StringSubsequence.c
@@ -1,30 +1,5 @@
 #include <stdio.h>
 int main(void){
-    //this prints the proper nouns in the sentence
-    /*const char* arr = "HelLo iM Jack and You ";
-    char wordarr[10];
-    int i;
-    int j=0;
-    while(arr[j]!='\0'){
-        i=0;
-        while(arr[j]!=' '){
-            wordarr[i]=arr[j];
-            i++;
-            j++;
-        }
-        if(wordarr[0]>='A' && wordarr[0]<='Z'){
-            for(int k=0; k<i; k++){
-                printf("%c", wordarr[k]);
-                wordarr[k]='!';
-            }
-            printf("\n");
-        }
-        j++;
-    }*/
-    /*char* str = "abcde";
-    printf("before: %s\n", str);
-    str[1] = 'g';
-    printf("after: %c\n", str[1]);*/
     int i=0;
     int j=0;
     int counter=0;
